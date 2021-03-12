27674abec9bec77cba78076499c81e93d8ad6334
dayfundora <dayfundoraglez@gmail.com>
2021-03-12 00:20:49 -0500

    Get Words Dict

--- anagrams_finder.py
+++ anagrams_finder.py
@@ -1,4 +1,18 @@
+from __future__ import print_function
+
+import sys
+import collections
 import char_ocurrence as cO
+
+def get_words_dict(file_):
+    result_dict = collections.defaultdict(set)
+
+    for candidate_line in file_:
+        candidate_word = candidate_line.lower().rstrip()
+        length = len(candidate_word)
+        result_dict[length].add(candidate_word)
+
+    return result_dict
 
 def find_anagrams(sentence):
     result_anagrams = []
@@ -6,5 +20,8 @@ def find_anagrams(sentence):
     sentence_list = list(sentence)
 
     sentence_frequencies = cO.CharOcurrence(sentence_list)
+    
+    with open('english.txt', 'r') as dictionary_file:
+        original_dict = get_words_dict(dictionary_file)
 
     return result_anagrams
