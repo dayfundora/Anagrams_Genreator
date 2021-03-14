475f8fcbde2904f602c2fa32088ea325df944361
dayfundora <dayfundoraglez@gmail.com>
2021-03-14 01:41:29 -0400

    Is Subset

--- char_ocurrence.py
+++ char_ocurrence.py
@@ -6,4 +6,17 @@ class CharOcurrence(object):
         self.frequencies = collections.Counter(sentence)
         if ' ' in self.frequencies:
             del self.frequencies[' ']
-   
+
+    def is_subset(self, other):
+        self_keys = set(self.frequencies)
+        other_keys = set(other.frequencies)
+
+        if self_keys <= other_keys:
+            for key in self_keys:
+                if self.frequencies[key] > other.frequencies[key]:
+                    return False
+            return True
+
+        return False
+
+  
