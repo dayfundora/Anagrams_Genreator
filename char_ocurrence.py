84180e1e3ad6631b06c43ce99166dbca07a83e12
dayfundora <dayfundoraglez@gmail.com>
2021-03-18 01:10:51 -0400

    Coments

--- char_ocurrence.py
+++ char_ocurrence.py
@@ -1,13 +1,16 @@
 import collections
 
 class CharOcurrence(object):
+    """Collection of char ocurrence in a sentence"""
 
     def __init__(self, sentence):
+        """Initialize a CharOcurrence."""
         self.frequencies = collections.Counter(sentence)
         if ' ' in self.frequencies:
             del self.frequencies[' ']
 
     def __sub__(self, other):
+        """Subtract two CharOcurrence"""
         result = CharOcurrence('')
 
         result.frequencies = self.frequencies - other.frequencies
@@ -15,6 +18,7 @@ class CharOcurrence(object):
         return result
 
     def is_subset(self, other):
+        """Return True iff self is a subset of other."""
         self_keys = set(self.frequencies)
         other_keys = set(other.frequencies)
 
