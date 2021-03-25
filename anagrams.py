731560eb44e1bc8f915f073ae7c96c609f337f1a
dayfundora <dayfundoraglez@gmail.com>
2021-03-25 00:12:39 -0400

    Words to Exclude

--- anagrams.py
+++ anagrams.py
@@ -3,18 +3,20 @@ import anagrams_finder
 
 
 def main():
-    sentence, include_words = [], []
+    sentence, exclude_words, include_words = [], [], []
 
     for word in sys.argv[1:]:
         
-        if(word.startswith('+')):
+        if(word.startswith('-')):
+            exclude_words.append(word[1:].lower())
+        elif(word.startswith('+')):
             include_words.append(word[1:].lower())
         else:
             sentence.append(word.lower())
 
     original_sentence = ' '.join(word.strip() for word in sys.argv[1:]).lower()
     
-    anagrams_finder.find_anagrams(original_sentence, include=include_words)
+    anagrams_finder.find_anagrams(original_sentence, include=include_words, exclude=exclude_words)
 
 
 main()
