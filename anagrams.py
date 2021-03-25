701c754ef8fe4d298c2706059957f66bd9aa161b
dayfundora <dayfundoraglez@gmail.com>
2021-03-25 00:12:14 -0400

    Words to Include

--- anagrams.py
+++ anagrams.py
@@ -3,9 +3,18 @@ import anagrams_finder
 
 
 def main():
+    sentence, include_words = [], []
+
+    for word in sys.argv[1:]:
+        
+        if(word.startswith('+')):
+            include_words.append(word[1:].lower())
+        else:
+            sentence.append(word.lower())
+
     original_sentence = ' '.join(word.strip() for word in sys.argv[1:]).lower()
     
-    anagrams_finder.find_anagrams(original_sentence)
+    anagrams_finder.find_anagrams(original_sentence, include=include_words)
 
 
 main()
