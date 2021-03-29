bddcb938312ed75b90f815e9da5660317a75341b
dayfundora <dayfundoraglez@gmail.com>
2021-03-29 00:37:54 -0400

    Fix Error from -include & exclude changes-

--- src/anagrams.py
+++ src/anagrams.py
@@ -14,7 +14,7 @@ def main():
         else:
             sentence.append(word.lower())
 
-    original_sentence = ' '.join(word.strip() for word in sys.argv[1:]).lower()
+    original_sentence = ' '.join(word.strip() for word in sentence).lower()
     
     anagrams_finder.find_anagrams(original_sentence, include=include_words, exclude=exclude_words)
 
